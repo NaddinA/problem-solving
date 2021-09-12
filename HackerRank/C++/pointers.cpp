e20274fbd0f287a4eefdf95c0d77e5c63e8d4433
Nadine Amr <69207331+NaddinA@users.noreply.github.com>
2021-09-12 02:57:32 +0200

    Create pointers.cpp

---
+++ HackerRank/C++/pointers.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+void update(int *a, int *b) {
+    int x, y, z;
+    
+    x = *a;
+    y = *b;
+    
+    z = x + y;
+    *a = z;
+    
+    *b = abs(x - y);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
